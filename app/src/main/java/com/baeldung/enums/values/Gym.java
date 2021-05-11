b0d18c9c187019a4534c513da55e741984cfee5e
Christa Meelker <christa.meelker@postnl.nl>
2021-05-11 21:48:56 +0200

    added bussum

--- app/src/main/java/com/baeldung/enums/values/Gym.java
+++ app/src/main/java/com/baeldung/enums/values/Gym.java
@@ -5,7 +5,8 @@ import com.example.chroslog.R;
 public enum Gym {
     Energiehaven_buiten   ("Energiehaven - buiten", 11,67, R.mipmap.eh_outdoor),
     Sterk  ("Sterk", 20,4,  R.mipmap.sterk),
-    Energiehaven_binnen("Energiehaven - Binnen", 11,15, R.mipmap.eh_indoor)
+    Energiehaven_binnen("Energiehaven - Binnen", 11,15, R.mipmap.eh_indoor),
+    Bussum ("Rock Steady Bussum", 28, 3, R.mipmap.bussum)
 
     ; // semicolon needed when fields / methods follow
 
